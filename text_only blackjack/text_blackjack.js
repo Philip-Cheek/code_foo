74471288e2db28adf021dcfad20c5320e666876d
Philip Cheek <maccsphilipcheek@gmail.com>
2016-04-20 20:32:24 -0700

    bj

--- text_only blackjack/text_blackjack.js
+++ text_only blackjack/text_blackjack.js
@@ -37,58 +37,58 @@ var Deck = function(){
 	this.stack = [];
 }
 
-Deck.prototype.construct = function(){
-	var card_names = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-	var suite = "spades";
-	for (i = 0; i < 4; i++){
-		for (c = 0; c < cards.length; c++){
-			if (i == 2){
-				suite = "clubs"
-			}else if (i == 3){
-				suite = "hearts"
-			}else{
-				suite = "diamonds"
+	Deck.prototype.construct = function(){
+		var card_names = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+		var suite = "spades";
+		for (i = 0; i < 4; i++){
+			for (c = 0; c < cards.length; c++){
+				if (i == 2){
+					suite = "clubs"
+				}else if (i == 3){
+					suite = "hearts"
+				}else if (i == 4){
+					suite = "diamonds"
+				};
+				var card = new Card(card_names[c], suite)
+				this.stack.push(card)
 			};
-			var card = new Card(card_names[c], suite)
-			this.stack.push(card)
 		};
 	};
-};
 
-Deck.prototype.shuffle = function(){
-	var i = this.deck.length - 1;
-	var swap;
-	var temp;
+	Deck.prototype.shuffle = function(){
+		var i = this.deck.length - 1;
+		var swap;
+		var temp;
 
-	while (i > 0){
-		swap = Math.floor(Math.random() * i);
+		while (i > 0){
+			swap = Math.floor(Math.random() * i);
 
-		temp = this.stack[i];
-		this.stack[i] = this.stack[swap];
-		this.stack[swap] = temp;
+			temp = this.stack[i];
+			this.stack[i] = this.stack[swap];
+			this.stack[swap] = temp;
 
-		i--;
-	}
+			i--;
+		}
 
-	return this;
-};
+		return this;
+	};
 
-Deck.prototype.see = function(){
-	for (var i = 0; i < this.deck.length; i++){
-		console.log(this.stack[i].name.toString() + " of " + this.stack[i].suite);
-	}
+	Deck.prototype.see = function(){
+		for (var i = 0; i < this.deck.length; i++){
+			console.log(this.stack[i].name.toString() + " of " + this.stack[i].suite);
+		}
 
-	return this;
-};
+		return this;
+	};
 
-Deck.prototype.draw = function(){
-	return this.stack.pop();
-};
+	Deck.prototype.draw = function(){
+		return this.stack.pop();
+	};
 
-Deck.prototype.reset = function(){
-	this.stack = [];
-	return this;
-};
+	Deck.prototype.reset = function(){
+		this.stack = [];
+		return this;
+	};
 
 var Dealer = function(){
 	this.hand = [];
@@ -96,29 +96,29 @@ var Dealer = function(){
 	this.deck = new Deck();
 };
 
-Dealer.prototype.deal = function(){
-	this.deck.draw();
-	return this;
-}
+	Dealer.prototype.deal = function(){
+		this.deck.draw();
+		return this;
+	}
 
-Dealer.prototype.start = function(){
-	this.deck.construct();
-	this.deck.shuffle();
-	return this;
-};
+	Dealer.prototype.start = function(){
+		this.deck.construct();
+		this.deck.shuffle();
+		return this;
+	};
 
-Dealer.prototype.hit = function(){
-	this.hand.push(this.deal());
-	return this;
-});
+	Dealer.prototype.hit = function(){
+		this.hand.push(this.deal());
+		return this;
+	});
 
-Dealer.prototype.hand_value = function(){
-	return sum_hand(this.hand)
-};
+	Dealer.prototype.hand_value = function(){
+		return sum_hand(this.hand)
+	};
 
-Dealer.prototype.reveal_hand = function(){
+	Dealer.prototype.reveal_hand = function(){
 
-}
+	}
 
 var Player = function(name){
 	this.name = name;
@@ -127,23 +127,23 @@ var Player = function(name){
 
 }
 
-Player.prototype.see_hand = function(){
-	var arr;
-	for (var i = 0; i < this.hand.length; i++){
-		arr.push(this.hand[i].name.toString() + " of " + this.hand[i].suite);
+	Player.prototype.see_hand = function(){
+		var arr;
+		for (var i = 0; i < this.hand.length; i++){
+			arr.push(this.hand[i].name.toString() + " of " + this.hand[i].suite);
+		}
+
+		return arr;
 	}
 
-	return arr;
-}
+	Player.prototype.hand_value = function(){
+		return sum_hand(this.hand)
+	}
 
-Player.prototype.hand_value = function(){
-	return sum_hand(this.hand)
-}
-
-Player.prototype.hit = function(dealer_object)
-	this.hand.push(dealer_object.deal())
-	return this;
-}
+	Player.prototype.hit = function(dealer_object)
+		this.hand.push(dealer_object.deal())
+		return this;
+	}
 
 var end_user;
 
@@ -164,6 +164,7 @@ $(#start).click(function(){
 	dealer.start().hit()
 	end_user.hit()
 
+	$(#dough_box).html("<p class = 'user_dough'" + user.dough.toString() + "</p>")
 	var log = end_user.see_cards();
 	$(#log).append("<p>You dealer hath done dealt. Your cards:</p>")
 	for (var x = 0; x < log.length; x++){
@@ -171,21 +172,37 @@ $(#start).click(function(){
 	};
 	var sum = end_user.hand_value
 	if (typeof sum == 'object'){
-		$(#log).append("<p>" + from_21(sum['ace_1']).toString() + "f rom 21, if ace is 1</p>")
-		$(#log).append("<p>" + from_21(sum['ace_11']).toString() + "from 21, if ace is 11</p>")
+		$(#log).append("<p>" + from_21(sum['ace_1']).toString() + " from 21, if ace is 1</p>")
+		$(#log).append("<p>" + from_21(sum['ace_11']).toString() + " from 21, if ace is 11</p>")
 	}
 
 });
 
 $(#hit).click(function(){
 	end_user.hit()
+	var user_sum = end_user.sum_hand()
 
 	var dealer_quandary = dealer.hand_value();
 	if (typeof dealer_quandary == 'object'){
 		if (from_21(dealer_quandary['ace_11'] != 0){
+            dealer.hit();
+		}
+	}else{
+		dealer.hit();
+	}
+
+	if (typeof user_sum == 'object'){
+		if (from_21(dealer_quandary['ace_11'] < 21 && from_21(dealer_quandary['ace_1'] < 21){
+           console.log('you lose')
+		}else if (from_21(dealer_quandary['ace_11'] < 21 && from_21(dealer_quandary['ace_1'] < 21)
 
 		}
-	}
+	}else{
+		dealer.hit();
 });
 
+$(#bet_butt).click(function(){
+	var bet = $(#user_bet)
+	$(#bet_box).html(bet)
+}
 
